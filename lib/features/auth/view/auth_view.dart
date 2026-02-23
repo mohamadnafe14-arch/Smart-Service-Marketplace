7ae152c354f9f041078af96f4c90f8056d6ac131
mohamed <mnafe44@gmail.com>
2026-02-23 15:52:43 +0200

    adding auth body

--- lib/features/auth/view/auth_view.dart
+++ lib/features/auth/view/auth_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:smart_service_marketplace/features/auth/view/widgets/auth_body.dart';
 
 class AuthView extends StatelessWidget {
   const AuthView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SafeArea(
+      child: Scaffold(body: AuthBody()),
+    );
   }
 }
---
+++ lib/features/auth/view/widgets/auth_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthBody extends StatefulWidget {
+  const AuthBody({super.key});
+
+  @override
+  State<AuthBody> createState() => _AuthBodyState();
+}
+
+class _AuthBodyState extends State<AuthBody> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
