a43682e36c022d1272829ac330835ecef106d7e9
mohamed <mnafe44@gmail.com>
2026-02-25 11:46:47 +0200

    adding auth repo

---
+++ lib/features/auth/data/repo/auth_repo.dart
@@ -0,0 +1,21 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:smart_service_marketplace/core/errors/failure.dart';
+import 'package:smart_service_marketplace/features/auth/data/model/user.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, User>> login({
+    required String email,
+    required String password,
+  });
+  Future<Either<Failure, User>> register({
+    required String email,
+    required String password,
+    required String name,
+  });
+  Future<Either<Failure, Unit>> logout();
+  Future<Either<Failure, User>> getCurrentUser();
+  Future<Either<Failure, Unit>> forgotPassword({required String email});
+  Future<Either<Failure, User>> verifyCode({required String code});
+  Future<Either<Failure, User>> authWithGoogle();
+  Future<Either<Failure, User>> authWithgithub();
+}
