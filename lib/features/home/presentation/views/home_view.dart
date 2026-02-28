c82ed9fcd0f15c256949dc5dfda1f00b6bf8fa43
mohamed <mnafe44@gmail.com>
2026-02-28 19:15:39 +0200

    finishing auth feature

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,10 +1,12 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SafeArea(
+      child: Scaffold(),
+    );
   }
 }
