ba494180d447b90571d769d600996a92d16a4858
mohamed <mnafe44@gmail.com>
2026-03-10 20:45:43 +0200

    adding category item

---
+++ lib/features/services/presentation/view/widgets/category_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.isSelected,
+  });
+  final String title;
+  final IconData icon;
+  final bool isSelected;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.h),
+      decoration: BoxDecoration(
+        color: isSelected ? Colors.blue : Colors.grey,
+        borderRadius: BorderRadius.circular(20.r),
+        border: Border.all(color: Colors.black, width: 2.w),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(icon, color: Colors.black),
+          SizedBox(width: 10.w),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 16.sp,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
