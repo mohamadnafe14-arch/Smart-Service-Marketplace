a33ecf2102bd39302fca5814558ffaada4636021
mohamed <mnafe44@gmail.com>
2026-03-11 15:27:06 +0200

    modifiying provider details view

--- lib/features/services/presentation/view/provider_details_view.dart
+++ lib/features/services/presentation/view/provider_details_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:smart_service_marketplace/features/profile/data/model/user_information.dart';
 
 class ProviderDetailsView extends StatelessWidget {
-  const ProviderDetailsView({super.key});
+  const ProviderDetailsView({super.key, required this.userInformation});
+  final UserInformation userInformation;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const SafeArea(child: Scaffold());
   }
 }
