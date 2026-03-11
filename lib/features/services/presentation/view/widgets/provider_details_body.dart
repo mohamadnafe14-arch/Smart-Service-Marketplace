fbd2c887293575435f9c30cb107d4daa289d0c38
mohamed <mnafe44@gmail.com>
2026-03-11 15:31:28 +0200

    adding provider details body

---
+++ lib/features/services/presentation/view/widgets/provider_details_body.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:smart_service_marketplace/core/widgets/custom_button.dart';
+import 'package:smart_service_marketplace/core/widgets/provider_information_widget.dart';
+import 'package:smart_service_marketplace/features/profile/data/model/user_information.dart';
+
+class ProviderDetailsBody extends StatelessWidget {
+  const ProviderDetailsBody({super.key, required this.userInformation});
+  final UserInformation userInformation;
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+        left: 20.w,
+        right: 20.w,
+        top: 20.h,
+      ),
+      child: Column(
+        children: [
+          ProviderInformationWidget(userInformation: userInformation),
+          SizedBox(height: 20.h),
+          CustomButton(onPressed: () {}, text: "عمل اوردر"),
+        ],
+      ),
+    );
+  }
+}
