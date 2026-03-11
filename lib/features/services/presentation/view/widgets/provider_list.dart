0ae4be87539590b475d0b23b3204ce7835027065
mohamed <mnafe44@gmail.com>
2026-03-11 15:16:16 +0200

    adding provider list

---
+++ lib/features/services/presentation/view/widgets/provider_list.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:smart_service_marketplace/features/profile/data/model/user_information.dart';
+import 'package:smart_service_marketplace/features/services/presentation/view/widgets/provider_card.dart';
+
+class ProviderList extends StatelessWidget {
+  const ProviderList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList.builder(
+      itemBuilder: (context, index) => ProviderCard(
+        userInformation: UserInformation.fromJson({
+          "category": "السباكة",
+        }),
+      ),
+    );
+  }
+}
