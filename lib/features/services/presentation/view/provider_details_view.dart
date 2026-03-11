19fa2ed34d832f4317ff7e169c9d8a15c85e5583
mohamed <mnafe44@gmail.com>
2026-03-11 15:18:05 +0200

    adding provider details view

---
+++ lib/features/services/presentation/view/provider_details_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ProviderDetailsView extends StatelessWidget {
+  const ProviderDetailsView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
