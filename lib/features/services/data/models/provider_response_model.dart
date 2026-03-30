f779590d601ffc85b35e9e1b44ae7c096e6be76e
mohamed <mnafe44@gmail.com>
2026-03-30 10:27:30 +0200

    adding provider response body

---
+++ lib/features/services/data/models/provider_response_model.dart
@@ -0,0 +1,19 @@
+import 'package:smart_service_marketplace/features/services/data/models/get_provider.dart';
+import 'package:smart_service_marketplace/features/services/data/models/pagination_link.dart';
+
+class ProvidersResponseModel {
+  final List<GetProvider> providers;
+  final List<PaginationLink> pagination;
+  ProvidersResponseModel({required this.providers, required this.pagination});
+
+  factory ProvidersResponseModel.fromJson(Map<String, dynamic> json) {
+    return ProvidersResponseModel(
+      providers: (json['providers'] as List)
+          .map((e) => GetProvider.fromJson(e))
+          .toList(),
+      pagination: (json['pagination'] as List)
+          .map((e) => PaginationLink.fromJson(e))
+          .toList(),
+    );
+  }
+}
