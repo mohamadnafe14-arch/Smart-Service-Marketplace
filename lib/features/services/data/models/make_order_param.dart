692cfd2eebabd1aca5efc6efd03bbf65b28b2d5e
mohamed <mnafe44@gmail.com>
2026-04-10 17:52:07 +0200

    adding make order parameters

---
+++ lib/features/services/data/models/make_order_param.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/scheduler.dart';
+
+class MakeOrderParam {
+  final String providerId;
+  final String token;
+  final VoidCallback? pop;
+  const MakeOrderParam({
+    required this.providerId,
+    required this.token,
+    required this.pop,
+  });
+}
