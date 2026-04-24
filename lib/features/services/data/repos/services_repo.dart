35ae268bab5bd59b3934133b837c0f1ce16792c2
mohamed <mnafe44@gmail.com>
2026-04-24 10:58:26 +0300

    adding make order function

--- lib/features/services/data/repos/services_repo.dart
+++ lib/features/services/data/repos/services_repo.dart
@@ -2,6 +2,7 @@ import 'package:fpdart/fpdart.dart';
 import 'package:smart_service_marketplace/core/errors/failure.dart';
 import 'package:smart_service_marketplace/features/profile/data/model/user_information.dart';
 import 'package:smart_service_marketplace/features/services/data/models/provider_response_model.dart';
+
 abstract class ServicesRepo {
   Future<Either<Failure, ProvidersResponseModel>> getProvidersByCategory({
     required String category,
@@ -12,8 +13,10 @@ abstract class ServicesRepo {
     required String token,
     required String id,
   });
-  Future<Either<Failure,String>> makeOrder({
+  Future<Either<Failure, String>> makeOrder({
     required String token,
     required String id,
+    required String description,
+    required String phone,
   });
 }
