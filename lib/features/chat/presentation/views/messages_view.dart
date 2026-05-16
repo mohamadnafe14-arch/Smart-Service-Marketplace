9d09c61ef3a7a8ea9cdc5c69480c3c529c12475f
mohamed <mnafe44@gmail.com>
2026-05-16 08:18:40 +0300

    adding message view

---
+++ lib/features/chat/presentation/views/messages_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:smart_service_marketplace/features/chat/presentation/views/widgets/chat_body.dart';
+import 'package:smart_service_marketplace/features/chat/presentation/views/widgets/chat_bubble.dart';
+import 'package:smart_service_marketplace/features/chat/presentation/views/widgets/chat_bubble_for_friend.dart';
+import 'package:smart_service_marketplace/features/chat/presentation/views/widgets/messages_list.dart';
+//TODO: وريها لما تخلص الواجهة بتاعت الشات
+class MessagesView extends StatelessWidget {
+  const MessagesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Messages")),
+      body: MessagesList(
+        scrollController: ScrollController(),
+      ),
+    );
+  }
+}
