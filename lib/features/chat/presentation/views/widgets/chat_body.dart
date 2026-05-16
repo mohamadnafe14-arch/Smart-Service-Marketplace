cfbe460914e5c7ebb64ddff22b2e88f040a2b683
mohamed <mnafe44@gmail.com>
2026-05-16 08:15:00 +0300

    modifying chat list to have scroll controller

--- lib/features/chat/presentation/views/widgets/chat_body.dart
+++ lib/features/chat/presentation/views/widgets/chat_body.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:smart_service_marketplace/features/chat/presentation/views/widgets/chat_list.dart'; 
-class ChatBody extends StatelessWidget {
-  const ChatBody({
-    super.key,
-  });
+import 'package:smart_service_marketplace/features/chat/presentation/views/widgets/chat_list.dart';
+
+class ChatBody extends StatefulWidget {
+  const ChatBody({super.key});
+  @override
+  State<ChatBody> createState() => _ChatBodyState();
+}
+
+class _ChatBodyState extends State<ChatBody> {
+  final ScrollController _scrollController = ScrollController();
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(slivers: [ChatList()]);
+    return CustomScrollView(
+      slivers: [ChatList(scrollController: _scrollController)],
+    );
   }
 }
--- lib/features/chat/presentation/views/widgets/chat_list.dart
+++ lib/features/chat/presentation/views/widgets/chat_list.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:smart_service_marketplace/features/chat/presentation/views/widgets/chat_card.dart';
 
 class ChatList extends StatelessWidget {
-  const ChatList({super.key});
+  const ChatList({super.key, required this.scrollController});
+  final ScrollController scrollController;
 
   @override
   Widget build(BuildContext context) {
-    return SliverList.builder(
+    return ListView.builder(
+      controller: scrollController,
       itemBuilder: (context, index) => const ChatCard(),
       itemCount: 10,
     );
