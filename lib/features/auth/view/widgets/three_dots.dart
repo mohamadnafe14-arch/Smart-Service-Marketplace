de5f2d17ba5f0828deddefe11522542494a7b66a
Mohamed <mohamadnafe14@gmail.com>
2026-08-15 13:28:55 +0300

    adding three dots

---
+++ lib/features/auth/view/widgets/three_dots.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:smart_service_market_place/features/auth/view/widgets/dot.dart';
+
+class ThreeDots extends StatelessWidget {
+  const ThreeDots({super.key, required this.animation});
+  final Animation<double> animation;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(3, (index) {
+        double progress = animation.value;
+        double segment = progress * 3;
+        bool isActive = segment > index;
+        return Dot(isActive: isActive);
+      }),
+    );
+  }
+}
