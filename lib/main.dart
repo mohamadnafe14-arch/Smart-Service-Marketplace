179e740b60a883f03f040d7976f134e19d98cd63
Mohamed <mohamadnafe14@gmail.com>
2026-08-19 17:24:58 +0300

    adding firebase intilization and dependecy injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:smart_service_market_place/core/utils/app_router.dart';
+import 'package:smart_service_market_place/core/utils/dependecy_injection.dart';
+import 'package:smart_service_market_place/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  configureDependencies();
   runApp(const SmartServiceMarketPlaceApp());
 }
 
