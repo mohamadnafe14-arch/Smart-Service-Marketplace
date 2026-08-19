ca53bc9703ace98b9dcc39f569fa709a0b236b70
Mohamed <mohamadnafe14@gmail.com>
2026-08-19 14:11:03 +0300

    adding splash body

--- lib/features/auth/view/splash_view.dart
+++ lib/features/auth/view/splash_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:smart_service_market_place/features/auth/view/widgets/splash_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(body: SplashBody());
   }
 }
