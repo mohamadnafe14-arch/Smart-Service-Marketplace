39d816ed1d24a450603c439d5c8ba0eb0d2bc3ba
Mohamed <mohamadnafe14@gmail.com>
2026-08-20 20:35:18 +0300

    adding sign in body

---
+++ lib/features/auth/view/widgets/sign_in_body.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:smart_service_market_place/core/widgets/custom_button.dart';
+import 'package:smart_service_market_place/features/auth/view/widgets/custom_text_form_field.dart';
+import 'package:smart_service_market_place/features/auth/view/widgets/google_button.dart';
+
+class SignInBody extends StatefulWidget {
+  const SignInBody({super.key});
+  @override
+  State<SignInBody> createState() => _SignInBodyState();
+}
+
+class _SignInBodyState extends State<SignInBody> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  String? email, password;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        padding: EdgeInsets.only(
+          left: 10.w,
+          right: 10.w,
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              "البريد الالكتروني",
+              style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.h),
+            CustomTextFormField(
+              hintText: "example@gmail.com",
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "يرجي ادخال البريد الالكتروني";
+                }
+                if (!RegExp(
+                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+                ).hasMatch(value)) {
+                  return "يرجي ادخال بريد الالكتروني صحيح";
+                }
+                if (!value.endsWith("@stm.com")) {
+                  return "يرجي ادحال بريد ينتهي ب'@stm.com'";
+                }
+                return null;
+              },
+              onSaved: (value) {
+                email = value;
+              },
+              onChanged: (value) {
+                email = value;
+              },
+              icon: Icons.email,
+            ),
+            SizedBox(height: 10.h),
+            Text(
+              "كلمة المرور",
+              style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.h),
+            CustomTextFormField(
+              hintText: "ادخل كلمة المرور",
+              isPassword: true,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "يرجي ادخال كلمة المرور";
+                }
+                return null;
+              },
+              onSaved: (value) {
+                password = value;
+              },
+              onChanged: (value) {
+                password = value;
+              },
+              icon: Icons.lock,
+            ),
+            SizedBox(height: 10.h),
+            Row(
+              children: [
+                Expanded(
+                  child: CustomButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {}
+                    },
+                    text: "ابدأ الان",
+                    isLoading: false,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10.h),
+            Row(
+              children: [
+                Expanded(
+                  child: Divider(color: Colors.grey, thickness: 4.w),
+                ),
+                Text("يمكنك ايضا المتابعة باستخدام"),
+                Expanded(
+                  child: Divider(color: Colors.grey, thickness: 4.w),
+                ),
+              ],
+            ),
+            SizedBox(height: 10.h),
+            Row(
+              children: [
+                Expanded(
+                  child: GoogleButton(onPressed: () {}, isLoading: false),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
