0d01af339452524f9e7ca2dea1206dd142d1dc1a
Mohamed <mohamadnafe14@gmail.com>
2026-08-20 20:29:12 +0300

    adding choose role body

---
+++ lib/features/auth/view/widgets/choose_role_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChooseRoleBody extends StatefulWidget {
+  const ChooseRoleBody({super.key});
+
+  @override
+  State<ChooseRoleBody> createState() => _ChooseRoleBodyState();
+}
+
+class _ChooseRoleBodyState extends State<ChooseRoleBody> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
