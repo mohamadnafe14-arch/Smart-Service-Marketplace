ec5207cf069a8e2d5f06ad8a2c1814172fc8c393
Mohamed <mohamadnafe14@gmail.com>
2026-08-28 20:38:55 +0300

    adding provider home view

---
+++ lib/features/home/provider_home_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProviderHomeView extends StatefulWidget {
+  const ProviderHomeView({super.key});
+
+  @override
+  State<ProviderHomeView> createState() => _ProviderHomeViewState();
+}
+
+class _ProviderHomeViewState extends State<ProviderHomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
