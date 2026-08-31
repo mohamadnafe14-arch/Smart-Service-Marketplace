4e4724298e049792d8a7eeb34126eac9f382fa0f
Mohamed <mohamadnafe14@gmail.com>
2026-08-31 15:21:54 +0300

    adding custom provider drawer

--- lib/features/home/views/provider_home_view.dart
+++ lib/features/home/views/provider_home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smart_service_market_place/features/auth/viewmodel/cubit/auth_cubit.dart';
+import 'package:smart_service_market_place/features/profile/view/widgets/custom_provider_drawer.dart';
 
 class ProviderHomeView extends StatefulWidget {
   const ProviderHomeView({super.key});
@@ -11,7 +14,9 @@ class _ProviderHomeViewState extends State<ProviderHomeView> {
   int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
+    final token = (context.read<AuthCubit>().state as AuthSuccess).user.token;
     return Scaffold(
+      drawer: CustomProviderDrawer(token: token),
       body: IndexedStack(
         index: currentIndex,
         children: const [
---
+++ lib/features/profile/view/widgets/custom_provider_drawer.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:smart_service_market_place/features/profile/view/widgets/provider_information_body.dart';
+import 'package:smart_service_market_place/features/profile/model/models/address.dart';
+import 'package:smart_service_market_place/features/profile/model/models/rating.dart';
+import 'package:smart_service_market_place/features/profile/model/models/statistics.dart';
+import 'package:smart_service_market_place/features/profile/model/models/user_information.dart';
+
+class CustomProviderDrawer extends StatefulWidget {
+  const CustomProviderDrawer({super.key, required this.token});
+  final String token;
+  @override
+  State<CustomProviderDrawer> createState() => _CustomProviderDrawerState();
+}
+
+class _CustomProviderDrawerState extends State<CustomProviderDrawer> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ProviderInformationBody(
+        token: widget.token,
+        userInformation: UserInformation(
+          name: "name",
+          id: 1,
+          email: "email",
+          phone: "phone",
+          createdSince: "createdSince",
+          address: Address(
+            city: "city",
+            street: "street",
+            addressInDetails: "addressInDetails",
+          ),
+          statistics: Statistics(totalNumberOfOrders: 0, finishedOrders: 0),
+          rating: Rating(rate: 0, count: 0),
+        ),
+      ),
+    );
+  }
+}
