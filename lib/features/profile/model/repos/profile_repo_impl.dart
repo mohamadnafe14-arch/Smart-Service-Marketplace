f3f77ec13d616190659edc2d241ebc7126e7b6c7
Mohamed <mohamadnafe14@gmail.com>
2026-09-02 13:02:10 +0300

    adding profile repo implementation

---
+++ lib/features/profile/model/repos/profile_repo_impl.dart
@@ -0,0 +1,77 @@
+import 'package:dio/dio.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:injectable/injectable.dart';
+import 'package:smart_service_market_place/core/errors/failure.dart';
+import 'package:smart_service_market_place/core/errors/server_failure.dart';
+import 'package:smart_service_market_place/core/services/dio_service.dart';
+import 'package:smart_service_market_place/features/profile/model/models/user_information.dart';
+import 'package:smart_service_market_place/features/profile/model/models/user_update.dart';
+import 'package:smart_service_market_place/features/profile/model/repos/profile_repo.dart';
+
+@LazySingleton(as: ProfileRepo)
+class ProfileRepoImpl implements ProfileRepo {
+  final DioService _dioService;
+  ProfileRepoImpl(this._dioService);
+
+  @override
+  Future<Either<Failure, UserInformation>> getUserInformation({
+    required String token,
+  }) async {
+    try {
+      final response = await _dioService.get(
+        path: 'api/profile',
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      );
+      if (response.statusCode == 200) {
+        final userInformation = UserInformation.fromJson(
+          response.data['data']["user"],
+        );
+        return Right(userInformation);
+      } else {
+        return Left(
+          Failure(message: response.data['message'] ?? 'Unknown error'),
+        );
+      }
+    } on DioException catch (e) {
+      return Left(ServerFailuer.fromDioError(dioException: e));
+    } catch (e) {
+      return Left(Failure(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, UserInformation>> updateUserInformation({
+    required UserUpdate userUpdate,
+    required String token,
+  }) async {
+    try {
+      final response = await _dioService.put(
+        path: 'api/profile',
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: userUpdate.toMap(),
+      );
+      if (response.statusCode == 200) {
+        final userInformation = UserInformation.fromJson(
+          response.data['data']["profile"],
+        );
+        return Right(userInformation);
+      } else {
+        return Left(
+          Failure(message: response.data['message'] ?? 'Unknown error'),
+        );
+      }
+    } on DioException catch (e) {
+      return Left(ServerFailuer.fromDioError(dioException: e));
+    } catch (e) {
+      return Left(Failure(message: e.toString()));
+    }
+  }
+}
