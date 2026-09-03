c8dfb929d02c18fe8046bde0e1e6f59404614bd9
Mohamed <mohamadnafe14@gmail.com>
2026-09-04 01:30:16 +0300

    adding category item

---
+++ lib/features/services/view/widgets/category_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:smart_service_marketplace/features/services/data/models/category_model.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({
+    super.key,
+    required this.categoryModel,
+    required this.isSelected,
+  });
+  final CategoryModel categoryModel;
+  final bool isSelected;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.h),
+      decoration: BoxDecoration(
+        color: isSelected ? Colors.blue : Colors.white,
+        borderRadius: BorderRadius.circular(20.r),
+        border: Border.all(color: Colors.black, width: 2.w),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(categoryModel.icon, color: Colors.black),
+          SizedBox(width: 10.w),
+          Text(
+            categoryModel.title,
+            style: TextStyle(
+              fontSize: 16.sp,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
