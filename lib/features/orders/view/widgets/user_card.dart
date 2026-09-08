1c3b6a1aa807a4dce84b1ffeea89d21bfb8f3f04
Mohamed <mohamadnafe14@gmail.com>
2026-09-08 18:40:12 +0300

    adding user card

---
+++ lib/features/orders/view/widgets/user_card.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:smart_service_market_place/core/functions/get_color_by_status.dart';
+import 'package:smart_service_market_place/features/orders/model/order_model.dart';
+import 'package:smart_service_market_place/features/orders/view/widgets/custom_action_button.dart';
+
+class UserCard extends StatelessWidget {
+  final OrderModel order;
+  const UserCard({super.key, required this.order});
+
+  @override
+  Widget build(BuildContext context) {
+    final statusColor = getColorByStatus(order.status);
+
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.w, horizontal: 12.w),
+      padding: EdgeInsets.all(16.h),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16.r),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.05),
+            blurRadius: 10,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                order.providerName,
+                style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.bold),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 6.h),
+                decoration: BoxDecoration(
+                  color: statusColor.withValues(alpha: 0.1),
+                  borderRadius: BorderRadius.circular(20.r),
+                ),
+                child: Text(
+                  order.status,
+                  style: TextStyle(
+                    color: statusColor,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 8.h),
+          SizedBox(height: 10.h),
+          Text(order.description),
+          SizedBox(height: 10.h),
+          Text(
+            order.updatedAt,
+            style: TextStyle(color: Colors.grey[500], fontSize: 12.sp),
+          ),
+          SizedBox(height: 15.h),
+          order.status == "active"
+              ? Row(
+                  children: [
+                    Expanded(
+                      child: CustomActionButton(
+                        text: "press here if the order is completed",
+                        color: Colors.green,
+                        onTap: () async {
+                          //TODO: Implement the logic to mark the order as completed
+                        },
+                      ),
+                    ),
+                  ],
+                )
+              : Container(),
+        ],
+      ),
+    );
+  }
+}
